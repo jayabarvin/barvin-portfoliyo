272d0bce7b1a0836bb5e9b613a12b0bfb81ee9ce
jayabarvin <jayabarvin2003@gmail.com>
2025-03-15 00:38:16 +0530

    fix:change the resume url

--- src/Componenets/Hero/Hero.jsx
+++ src/Componenets/Hero/Hero.jsx
@@ -10,8 +10,7 @@ function Hero(){
         <h1><span>I'm JayaBarvin,</span> React Developer.</h1>
         <p>I am a React And web FullStack Developer</p>
         <div className="hero-action">
-            <div className="hero-resume"><a href="https://firebasestorage.googleapis.com/v0/b/resume-20112003.appspot.com/o/Jayabarvin_B.pdf?alt=media&token=44763fc9-7989-4326-8176-da05b6327f26" target="_blank" rel="noreferrer" style={{color:"white"}}>My Resume</a></div>
-            {/* <div className="hero-resume"></div> */}
+            <div className="hero-resume"><a href="https://firebasestorage.googleapis.com/v0/b/resume-20112003.appspot.com/o/barvinSyncfusion%2FJayabarvin%20B.pdf?alt=media&token=ed09773d-b433-4827-8164-d829e3683e5b" target="_blank" rel="noreferrer" style={{color:"white"}}>My Resume</a></div>
         </div>
         </div>
     );
